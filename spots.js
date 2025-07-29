a21b9465035f203a4999e52576bbf2125e89b1ab
Geoffrey Vidal <geoffreyvidal@MacBook-Air-de-Geoffrey.local>
2025-07-29 11:49:11 +0200

    feat: Add Bidart spot

--- spots.js
+++ spots.js
@@ -4,6 +4,12 @@ const spots = [
         name: 'Biarritz - La Côte des Basques',
         pageUrl: 'https://gosurf.fr/webcam/fr/7/Biarritz-La-Cote-des-Basques',
         streamUrl: 'https://ds1-cache.quanteec.com/contents/encodings/live/4ec957fc-fea2-49f2-3530-3130-6d61-63-872c-0869d3d9cef1d/media_1.m3u8'
+    },
+    {
+        id: 'bidart-plage-du-centre',
+        name: 'Bidart - Plage du Centre',
+        pageUrl: 'https://gosurf.fr/webcam/fr/147/Bidart-Plage-du-Centre',
+        streamUrl: 'https://ds2-cache.quanteec.com/contents/encodings/live/36f2d43a-6345-4afd-3031-3130-6d61-63-8139-e6d142b8a4c3d/master.m3u8'
     }
     // Vous pourrez ajouter d'autres spots ici
 ];
