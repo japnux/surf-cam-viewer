c20aa3dccd1e898b2ee5949e1186ff7c9da9ae84
Geoffrey Vidal <geoffreyvidal@MacBook-Air-de-Geoffrey.local>
2025-07-29 12:38:48 +0200

    feat: Enrich spot data with precise location info

--- spots.js
+++ spots.js
@@ -2,12 +2,25 @@ const spots = [
     {
         id: 'biarritz-cote-des-basques',
         name: 'Biarritz - La Côte des Basques',
+        city: 'Biarritz',
+        department: '64',
+        location: {
+            lat: 43.47904830531981,
+            lng: -1.5669167662795969 
+        },
         pageUrl: 'https://gosurf.fr/webcam/fr/7/Biarritz-La-Cote-des-Basques',
         streamUrl: 'https://ds1-cache.quanteec.com/contents/encodings/live/4ec957fc-fea2-49f2-3530-3130-6d61-63-872c-0869d3d9cef1d/media_1.m3u8'
     },
     {
         id: 'bidart-plage-du-centre',
         name: 'Bidart - Plage du Centre',
+        city: 'Bidart',
+        department: '64',
+        location: {
+
+            lat: 43.437646629143664, 
+            lng: -1.5961215676667442
+        },
         pageUrl: 'https://gosurf.fr/webcam/fr/147/Bidart-Plage-du-Centre',
         streamUrl: 'https://ds2-cache.quanteec.com/contents/encodings/live/36f2d43a-6345-4afd-3031-3130-6d61-63-8139-e6d142b8a4c3d/master.m3u8'
     }
