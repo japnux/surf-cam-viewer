1c8b6f443b58b70dbf50838762c4302532bcbfb3
Geoffrey Vidal <geoffreyvidal@MacBook-Air-de-Geoffrey.local>
2025-07-29 11:42:39 +0200

    Initial commit

---
+++ spots.js
@@ -0,0 +1,9 @@
+const spots = [
+    {
+        id: 'biarritz-cote-des-basques',
+        name: 'Biarritz - La Côte des Basques',
+        pageUrl: 'https://gosurf.fr/webcam/fr/7/Biarritz-La-Cote-des-Basques',
+        streamUrl: 'https://ds1-cache.quanteec.com/contents/encodings/live/4ec957fc-fea2-49f2-3530-3130-6d61-63-872c-0869d3d9cef1d/media_1.m3u8'
+    }
+    // Vous pourrez ajouter d'autres spots ici
+];
